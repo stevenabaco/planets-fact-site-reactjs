2e7e55b14c890ca8f0b74a5554124c25be5cdce1
Steven Abaco <stevenabaco@gmail.com>
2021-09-02 11:45:48 -0500

    Started Desktop styling

--- src/components/Planets/Planet.js
+++ src/components/Planets/Planet.js
@@ -14,24 +14,26 @@ function Planet() {
 					<div id='radio__toolbar' className='flex'>
 						{ContentTabs.map(
 							({ id, name, label, value, valueimg, defaultChecked }) => (
-								<div id='contentSelector' key={id}>
+								<div className='categorySelector' key={id}>
 									<form>
-										<input
-											type='radio'
-											key={id}
-											id={id}
-											name={name}
-											value={value.toString()}
-											valueimg={valueimg}
-											defaultChecked={defaultChecked}
-											className={context.currentPlanet}
-											onChange={() => {
-												context.setCategory(label);
-											}}
-											tabIndex={+id}
-											checked={context.category === label}
-										/>
-										<label htmlFor={id}>{label}</label>
+										
+											<input
+												type='radio'
+												key={id}
+												id={id}
+												name={name}
+												value={value.toString()}
+												valueimg={valueimg}
+												defaultChecked={defaultChecked}
+												className={context.currentPlanet}
+												onChange={() => {
+													context.setCategory(label);
+												}}
+												tabIndex={+id}
+												checked={context.category === label}
+											/>
+											<label htmlFor={id}>{label}</label>
+						
 									</form>
 								</div>
 							)
@@ -66,41 +68,53 @@ function Planet() {
 					<div id='planet__content'>
 						<h1>{context.currentPlanet}</h1>
 						{
-							<p>
-								{context.category === 'Overview' &&
-									context.data[2].overview.content}
-							</p>
+							<div>
+								<p>
+									{context.category === 'Overview' &&
+										context.data[2].overview.content}
+								</p>
+							</div>
 						}
 						{
-							<p>
-								{context.category === 'Structure' &&
-									context.data[context.planetIndex].structure.content}
-							</p>
+							<div>
+								<p>
+									{context.category === 'Structure' &&
+										context.data[context.planetIndex].structure.content}
+								</p>
+							</div>
 						}
 						{
-							<p>
-								{context.category === 'Surface' &&
-									context.data[context.planetIndex].geology.content}
-							</p>
+							<div>
+								<p>
+									{context.category === 'Surface' &&
+										context.data[context.planetIndex].geology.content}
+								</p>
+							</div>
 						}
 						{context.category === 'Overview' ? (
 							<p>
 								Source:{' '}
-								<a class="link" href={context.data[context.planetIndex].overview.source}>
+								<a
+									class='link'
+									href={context.data[context.planetIndex].overview.source}>
 									Wikipedia
 								</a>
 							</p>
 						) : context.category === 'Structure' ? (
 							<p>
 								Source:{' '}
-								<a class="link" href={context.data[context.planetIndex].structure.source}>
+								<a
+									class='link'
+									href={context.data[context.planetIndex].structure.source}>
 									Wikipedia
 								</a>
 							</p>
 						) : (
 							<p>
 								Source:{' '}
-								<a class="link" href={context.data[context.planetIndex].geology.source}>
+								<a
+									class='link'
+									href={context.data[context.planetIndex].geology.source}>
 									Wikipedia
 								</a>
 							</p>
@@ -120,7 +134,7 @@ function Planet() {
 							<span>{context.data[context.planetIndex].radius}</span>
 						</div>
 						<div id='temp' className='planet__facts flex'>
-							<p>Average Temp</p>
+							<p>Average Temp.</p>
 							<span>{context.data[context.planetIndex].temperature}</span>
 						</div>
 					</div>
